39908e759eb2749e4af5d1f1d3a861fbf16da890
Bryan DiSanto <bryan.disanto@gmail.com>
2026-02-13 12:29:20 -0400

    fix: add support for UPSTASH_REDIS env vars

--- lib/kv.ts
+++ lib/kv.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@vercel/kv';
 
 // Custom KV client configuration
-const url = process.env.MY_KV_URL || process.env.KV_REST_API_URL;
-const token = process.env.MY_KV_TOKEN || process.env.KV_REST_API_TOKEN;
+const url = process.env.MY_KV_URL || process.env.KV_REST_API_URL || process.env.UPSTASH_REDIS_REST_URL;
+const token = process.env.MY_KV_TOKEN || process.env.KV_REST_API_TOKEN || process.env.UPSTASH_REDIS_REST_TOKEN;
 
 console.log('[kv] Using URL:', url);
 
